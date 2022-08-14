819655a5c7237f1a3f9b4c7b26d089bc20ac152c
Brajesh Mishra <brajeshmishra825@gmail.com>
2022-08-14 22:12:09 +0530

    Create and move grid of invaders

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ class Player {
     };
     this.rotation = 0;
     const image = new Image();
-    // image.src = "./img/spaceship.png";
-    image.src = "./img/rocket.png";
+    image.src = "./img/spaceship.png";
+    // image.src = "./img/rocket.png";
 
     image.onload = () => {
-      const scale = 0.075;
+      const scale = 0.15;
       this.image = image;
       this.width = image.width * scale;
       this.height = image.height * scale;
@@ -65,51 +65,6 @@ class Player {
   }
 }
 
-class Invader {
-  constructor() {
-    this.velocity = {
-      x: 0,
-      y: 0,
-    };
-    const image = new Image();
-    // image.src = "./img/spaceship.png";
-    image.src = "./img/invader.png";
-    image.onload = () => {
-      const scale = 1;
-      this.image = image;
-      this.width = image.width * scale;
-      this.height = image.height * scale;
-      this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: canvas.height / 2,
-      };
-    };
-  }
-
-  draw() {
-    // c.fillStyle = "red";
-    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-    if (this.image) {
-      c.drawImage(
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
-    }
-  }
-  update() {
-    if (this.image) {
-      this.draw();
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
-      this.radius = 3;
-    }
-  }
-}
-
 class Projectile {
   constructor({ position, velocity }) {
     this.position = position;
@@ -133,9 +88,100 @@ class Projectile {
   }
 }
 
+class Invader {
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    const image = new Image();
+    // image.src = "./img/spaceship.png";
+    image.src = "./img/invader.png";
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
+  draw() {
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    if (this.image) {
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+      this.radius = 3;
+    }
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 3,
+      y: 1,
+    };
+    this.invaders = [];
+
+    const columns = Math.floor(Math.random() * 10) + 5;
+    const rows = Math.floor(Math.random() * 5) + 2;
+
+    this.width = columns * 30;
+
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: i * 30,
+              y: j * 30,
+            },
+          })
+        );
+      }
+    }
+  }
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0;
+
+    if (this.position.x + this.width >= canvas.width) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 60;
+    } else if (this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 60;
+    }
+  }
+}
+
+const grids = [new Grid()];
+
 const player = new Player();
 const projectiles = [];
-const invader = new Invader();
 
 const keys = {
   a: {
@@ -151,7 +197,12 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  invader.update();
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
+  });
 
   player.update();
 
